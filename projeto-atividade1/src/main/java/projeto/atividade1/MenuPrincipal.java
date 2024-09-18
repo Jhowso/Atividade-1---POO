6f531526d8a9925d96bb8b03d88549ffacb647b5
Ryan da Silva Doriguetto <ryandasilvadoriguetto@gmail.com>
2024-09-18 20:10:40 -0300

    Update and rename Main.java to MenuPrincipal.java

---
+++ projeto-atividade1/src/main/java/projeto/atividade1/MenuPrincipal.java
@@ -0,0 +1,11 @@
+package folhadepagamento;
+import java.util.Scanner;
+public class MenuPrincipal extends Funcionario {
+    String nome
+    Scanner ler = new Scanner(System.in);
+
+    private static void cadastrarFunc() {
+        System.out.println("Qual o nome do colaborador?");
+
+    }
+}
